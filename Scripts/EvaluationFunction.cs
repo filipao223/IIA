5204fe6054a4d9fa58d0f32d0226619c13d2364e
OverSkillers <overskilers@gmail.com>
2019-04-06 03:45:28 +0100

    Nova funçao de avaliaçao

--- Scripts/EvaluationFunction.cs
+++ Scripts/EvaluationFunction.cs
@@ -16,53 +16,77 @@ public class EvaluationFunction
         //Check our total health
         List<Unit> ourUnits = s.PlayersUnits;
         float ourHealth = 0.0F;
+        float theirHealth = 0.0F;
         foreach (Unit unit in ourUnits)
         {
-            ourHealth += unit.hp;
+            if(unit is Assassin){
+                ourHealth += 0.9f*unit.hp;
+            }
+            if(unit is Mage){
+                ourHealth += 0.7f*unit.hp;
+            }
+            if(unit is Protector || unit is Warrior){
+                ourHealth += 0.5f*unit.hp;
+            }
             Tuple<float, float> tuple = unit.GetBonus(s.board,ourUnits);
             //verifica se está perto de um warrior ou protector e valoriza isso
             if(tuple.Item1 != 0){
-                ourHealth = ourHealth + tuple.Item1;
+                if(unit is Assassin){
+                    ourHealth += 0.2f*tuple.Item1;
+                }
+                if(unit is Mage){
+                    ourHealth += 0.3f*tuple.Item1;
+                }
+                if(unit is Protector || unit is Warrior){
+                    ourHealth += 0.4f*tuple.Item1;
+                }
             }
             if(tuple.Item2 != 0){
-                ourHealth = ourHealth + tuple.Item2;
+                if(unit is Assassin){
+                    ourHealth += 0.2f*tuple.Item1;
+                }
+                if(unit is Mage){
+                    ourHealth += 0.3f*tuple.Item1;
+                }
+                if(unit is Protector || unit is Warrior){
+                    ourHealth += 0.4f*tuple.Item1;
+                }
             }
+
+            List<Unit> attackable = new List<Unit>();
+
+            attackable = unit.GetAttackable();
+
+            if(attackable.Count != 0){
+                       foreach(Unit u in attackable){
+                           if((unit.attack + unit.attackbonus) >= 2f*(u.attack+u.attackbonus)){
+                               theirHealth -= (unit.attack + unit.attackbonus);
+                           }
+                           if(ourUnits.Count > attackable.Count && (unit.hp + unit.hpbonus)>(u.attack+u.attackbonus)){
+                               theirHealth -= (unit.attack + unit.attackbonus);
+                           }
+                       }
+            }
+
         }
 
         //Check adversary total health
         List<Unit> theirUnits = s.AdversaryUnits;
-        float theirHealth = 0.0F;
         foreach (Unit unit in theirUnits)
         {
-            theirHealth += unit.hp;
+            if(unit is Assassin){
+                theirHealth += 0.9f*unit.hp;
+            }
+            if(unit is Mage){
+                theirHealth += 0.7f*unit.hp;
+            }
+            if(unit is Protector || unit is Warrior){
+                theirHealth += 0.5f*unit.hp;
+            }
+
         }
 
         float healthScore = ourHealth - theirHealth;
-
-
-        int gridSizeX = s.board.GetLength(0);
-        int gridSizeY = s.board.GetLength(1);
-
-        /*//Check if we are in attack range of an oponent
-        foreach (Unit unit in ourUnits)
-        {
-            //Code copied from Unit class, GetAttackable method
-            for (int i = 0; i < unit.attackrange.GetLength(0); i++)
-            {
-                int checkX = unit.x + unit.attackrange[i, 0];
-                int checkY = unit.y + unit.attackrange[i, 1];
-                if (checkX >= 0 && checkX < gridSizeX && checkY >= 0 && checkY < gridSizeY)
-                {
-                    Unit neighbour = s.board[checkX, checkY];
-                    if (neighbour != null && theirUnits.Contains(neighbour) && !neighbour.IsDead())
-                    {
-                        healthScore *= unit.attack/10;
-                        break;
-                    }
-                    //if(unit) //colocar a valorizar o ataque do mage
-                }
-            }
-        }*/
 
         return healthScore;
     }
--- Scripts/UtilityFunction.cs
+++ Scripts/UtilityFunction.cs
@@ -9,32 +9,5 @@ public class UtilityFunction
 
     public float evaluate(State s)
     {
-        /////////////////
-        // You should implement these
-        /////////////////
-
-        List<Unit> theirUnits = s.AdversaryUnits;
-        float theirHealth = 0.0F;
-        foreach (Unit unit in theirUnits)
-        {
-            theirHealth += unit.hp;
-        }
-
-        if (theirHealth == 0)
-        {
             return Int32.MaxValue;
-        }
-
-        List<Unit> ourUnits = s.PlayersUnits;
-        float ourHealth = 0.0F;
-        foreach (Unit unit in ourUnits){
-            ourHealth += unit.hp;
-        }
-
-        if (ourHealth == 0){
-            return Int32.MinValue;
-        }
-
-        return 0;
     }
-}
